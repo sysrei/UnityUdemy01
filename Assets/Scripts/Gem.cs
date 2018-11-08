a0bb6c4e5a8007d44b256925455d7cb8c06b4e9f
Marcelo Bareisys <marcelo-bareisys@prodesan.com.br>
2018-11-08 16:03:55 -0200

    Working on items and starting a script

---
+++ Assets/Scripts/Gem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void Awake(){
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//For objects with phisics like rigidbody
+	void FixedUpdate(){
+
+	}
+	void OnTriggerEnter2D(Collider2D collider){
+
+	}
+}
