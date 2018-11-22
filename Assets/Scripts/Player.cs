6702eb69d76017fba8b49e340fba1a40bf2429db
Marcelo Bareisys <marcelo-bareisys@prodesan.com.br>
2018-11-22 15:55:32 -0200

    Added jump and movement to the player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,32 +14,63 @@ private bool jumping; //Know if the player is jumping
 
 public float radiusCheck;
 
-private RigidBody2D rb2D;
+private Rigidbody2D rb2D;
+private Animator animator;
+private bool facingRight = true;
 
 	// Use this for initialization
 	void Start () {
-		rb2D = GetComponent<RigidBody2D>();
+		rb2D = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		grounded = Physics2D.OverlapCircle(groundCheck.position, radiusCheck, layerGround);
+
+		//???????? Colocar no fixed para testar
+		if(Input.GetButtonDown("Jump") && grounded){
+			//Jump commands
+			jumping = true;
+		}
+		PlayAnimations();
 	}
 
 	void FixedUpdate(){
 
+		float move = 0f;
 
+		move = Input.GetAxis("Horizontal");
+		rb2D.velocity = new Vector2(move * speed, rb2D.velocity.y);
 
+		if(move < 0 && facingRight || move > 0 && !facingRight){
+			Flip();
+		}
 
-
-
-
+		if(jumping){
+			rb2D.AddForce(new Vector2(0f, jumpPower));
+			jumping = false;
+		}
 	}
 
 	void PlayAnimations(){
 
+		if(grounded && rb2D.velocity.x != 0){
+		
+			animator.Play("Run");
+			
+		}
+		else if(grounded && rb2D.velocity.x == 0){
+			animator.Play("Idle");
+		}
+		else if(!grounded){
+			animator.Play("Jump");
+		}
 	}
 
-
+	void Flip(){
+		facingRight = !facingRight;
+		transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+	}
 
 }
