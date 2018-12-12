603d971e2ce9c90b947427734307e24f5e8a9da1
Marcelo Bareisys <sysrei@gmail.com>
2018-12-12 20:20:33 -0200

    Created Camera Routine

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -6,7 +6,7 @@ public class Camera : MonoBehaviour {
 
 	private Vector2 velocity;
 
-	public Transform targetPlayer;
+	public Transform target;
 	public Vector2 smoothTime;
 	public Vector2 maxLimit;
 	public Vector2 minLimit;
@@ -15,11 +15,18 @@ public class Camera : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		// transform.position = new Vector3(targetPlayer.position.x, targetPlayer.position.y, transform.position.z);
+		 transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (target != null)
+        {
+            float posX = Mathf.SmoothDamp(transform.position.x, target.position.x, ref velocity.x, smoothTime.x);
+            float posY = Mathf.SmoothDamp(transform.position.y, target.position.y, ref velocity.y, smoothTime.y);
+
+            transform.position = new Vector3(posX, posY, transform.position.z);
+        }
 		
 	}
 }
