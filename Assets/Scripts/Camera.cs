f77151ed0a3a5eff89979f21c6f15a4919a6fd32
Marcelo Bareisys <marcelo-bareisys@prodesan.com.br>
2018-12-12 15:48:00 -0200

    corrected camesra script

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -15,7 +15,7 @@ public class Camera : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		// transform.position = new Vector3(targetPlayer.position.x, targetPlayer.position.y, transform.position.z)
+		// transform.position = new Vector3(targetPlayer.position.x, targetPlayer.position.y, transform.position.z);
 	}
 	
 	// Update is called once per frame
