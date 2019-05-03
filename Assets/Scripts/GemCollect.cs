299d4f17afb88232eff4c044364b3164a24bd345
sysrei <sysrei@gmail.com>
2019-05-03 10:54:50 -0300

    added gemCollectScript

---
+++ Assets/Scripts/GemCollect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemCollect : MonoBehaviour {
+
+void OnTriggerEnter2D(Collider2D otherObject){
+	if(otherObject.CompareTag("Player"))
+	{
+		Destroy(gameObject);
+	}
+}
+
+}
